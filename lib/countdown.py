bc38e70c314981e1d9f592e4681ed8cfa04c9b8c
khalidabdikarim99 <12345678+khalidabdikarim99@users.noreply.github.com>
2025-06-17 04:57:32 -0700

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,17 +1,14 @@
-# your code goes here!
 import time
 
-def countdown(start):
-    """Counts down from start to 1, then prints Happy New Year!"""
-    while start > 0:
-        print(f"{start} SECOND(S)!")
-        start -= 1
+def countdown(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
     print("HAPPY NEW YEAR!")
 
-def countdown_with_sleep(start):
-    """Counts down from start to 1 with 1-second delays, then prints Happy New Year!"""
-    while start > 0:
-        print(f"{start} SECOND(S)!")
-        start -= 1
+def countdown_with_sleep(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
         time.sleep(1)
+        number -= 1
     print("HAPPY NEW YEAR!")
